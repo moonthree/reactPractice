22ca330dacba14a780ff3f6440a1b80a5961741b
moonthree <fpdhslr0725@gmail.com>
2022-12-04 16:33:54 +0900

    a

--- shoppy-final/src/index.js
+++ shoppy-final/src/index.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Home from './pages/Home';
-import AllProducts from './pages/AllProducts';
-import ProductDetail from './pages/ProductDetail';
-import NewProduct from './pages/NewProduct';
-import MyCart from './pages/MyCart';
-import NotFound from './pages/NotFound';
-import ProtectedRoute from './pages/ProtectedRoute';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import ProductDetail from "./pages/ProductDetail";
+import NewProduct from "./pages/NewProduct";
+import MyCart from "./pages/MyCart";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />,
+    path: "/",
+    element: <App />, // 최상위 경로에서는 App.js를 보여준다.
+    errorElement: <NotFound />, // 페이지가 존재하지 않을 경우에는 NotFound.jsx를 보여준다.
+    // App이라는 부모 컴포넌트 안에 outlet을 사용하여 children을 보여준다.
     children: [
-      { index: true, path: '/', element: <Home /> },
-      { path: '/products', element: <AllProducts /> },
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
       {
-        path: '/products/new',
+        path: "/products/new",
         element: (
           <ProtectedRoute requireAdmin>
             <NewProduct />
@@ -28,11 +29,11 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: '/products/:id',
+        path: "/products/:id",
         element: <ProductDetail />,
       },
       {
-        path: '/carts',
+        path: "/carts",
         element: (
           <ProtectedRoute>
             <MyCart />
@@ -43,7 +44,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
